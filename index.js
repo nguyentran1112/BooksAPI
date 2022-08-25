ad1536811a42c2c015c60d2010ed230967bdd18a
nguyentran <nt85272@gmail.com>
2022-08-25 10:11:07 +0700

    push update code

--- index.js
+++ index.js
@@ -8,25 +8,24 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const authorRouter = require("./routes/author");
 const bookRouter = require("./routes/book");
-dotenv.config()
-const port = 3333;
+dotenv.config();
 //connect Database
 
-mongoose.connect(
-  (process.env.MONGODB_URL),
-  () => {
-    console.log("Connected to MongoDB");
-  }
-);
+mongoose.connect(process.env.MONGODB_URL, () => {
+  console.log("Connected to MongoDB");
+});
 //code app
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
 app.use(morgan());
-app.use("/v1/author", authorRouter)
-app.use("/v1/book", bookRouter)
+app.use("/v1/author", authorRouter);
+app.use("/v1/book", bookRouter);
 app.get("/", (req, res) => {
   res.send("home");
 });
+//const port = 3333;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
